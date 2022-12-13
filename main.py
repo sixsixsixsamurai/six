a9b7e34b305c4e8632b6f701c5a7995ef24f5aa7
Mykhola <kradu2001@gmail.com>
2022-12-13 07:28:57 +0200

    Modify main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+a = int(input("Задача №1\nВведите целое число : "))
+
+if a % 2 == 1:
+    print("Weird")
+
+b = int(input("Задача №2\nВведите целое число : "))
+
+if b % 2 == 0 and b >= 2 and b <= 5:
+    print("Not Weird")
+
+c = int(input("Задача №3\nВведите целое число : "))
+
+if c % 2 == 0 and c >= 6 and c <= 20:
+    print("Weird")
+
+d = int(input("Задача №4\nВведите целое число : "))
+
+if d % 2 == 1 and d > 20:
+    print("Not Weird")
